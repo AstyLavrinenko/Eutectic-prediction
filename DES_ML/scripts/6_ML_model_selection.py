b9eec20d1af6777d7ca29a7b1cf45f1a01d3ae68
AstyLavrinenko <75916717+AstyLavrinenko@users.noreply.github.com>
2022-12-22 17:32:57 +0300

    Add files via upload

---
+++ DES_ML/scripts/6_ML_model_selection.py
@@ -0,0 +1,61 @@
+''' Comparison of ML models '''
+
+import fit_functions
+import add_descriptors
+
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+
+#%% Read data
+df = pd.read_csv('../descriptors/mixture/main.csv')
+df = add_descriptors.descriptors().add_selected_features(df)
+to_drop = ['Component#1','Smiles#1','Component#2','Smiles#2','T_EP','PD']
+df = pd.get_dummies(df, columns = ['Type'])
+y = df['T_EP']
+x = df.drop(to_drop, axis = 1)
+cv = fit_functions.custom_cv(x, y, df.groups, 1, 0.2)
+for train_idx, val_idx in cv:
+    x_train,x_val = x.iloc[train_idx], x.iloc[val_idx]
+    y_train,y_val = y.iloc[train_idx], y.iloc[val_idx]
+groups = x_train.groups
+x_train = x_train.drop(['groups'],axis=1)
+
+#%% Training of ML models
+models = {'RFR': RandomForestRegressor(max_depth=9, max_features=5, min_samples_leaf=0.02,
+                      min_samples_split=0.05, n_estimators=65, n_jobs=-1), 
+          'GBR': GradientBoostingRegressor(learning_rate=0.2, max_depth=6,
+                          max_features=8, min_samples_leaf=0.04,
+                          min_samples_split=0.1, n_estimators=45), 
+          'KNN': KNeighborsRegressor(metric='manhattan', n_neighbors=7), 
+          'SVR': SVR(C=5.0, gamma=1.0),
+          'MLR': LinearRegression()
+          }
+
+results = pd.DataFrame()
+idx=-1
+for name, model in models.items():
+    idx+=1
+    params = {'ML_model':model, 
+              'ML_name':name, 
+              'x_train':np.array(x_train), 
+              'groups': groups,
+              'y_train':np.array(y_train).reshape(-1, 1), 
+              'scaler_y':False, 
+              'scaler_x':MinMaxScaler(), 
+              'n_kfold':5,
+              'validation':True,
+              'x_val':x_val.drop(['groups'],axis=1),
+              'y_val':y_val}
+    if name == 'SVR':
+        params['scaler_y'] = True
+    result_dict = fit_functions.fit_model_kfold(**params)
+    for key,value in result_dict.items():
+        results.loc[idx,key] = str(value)
+results = results.sort_values(by=['rmse_val'])
+results.to_csv('../results/ML_model_selection.csv', index = False)
