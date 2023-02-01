6626f27baf4d27a36ee55e569bfc8f4158a7d089
AstyLavrinenko <75916717+AstyLavrinenko@users.noreply.github.com>
2023-02-01 11:44:53 +0300

    Delete _sup directory

--- _sup/data/SLE_COSMO/SLE_CA_17/SLE_CA.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-/home/nastya/COSMOlogic/COSMOthermX19/COSMOtherm/BIN-LINUX/cosmotherm /media/nastya/storage/Project_nastya/SLE_CA/NUPDFKWZQURWCC-UHFFFAOYSA-M_SGMZJAMFUVOLNK-UHFFFAOYSA-M.inp&
